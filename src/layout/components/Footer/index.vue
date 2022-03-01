f2af017925d95973d4246c0472e7ac4eb20c3c17
MTrun <1262327911@qq.com>
2022-03-01 14:09:34 +0800

    fix： 修改 footer 位置的 doc 链接地址

--- src/layout/components/Footer/index.vue
+++ src/layout/components/Footer/index.vue
@@ -3,15 +3,17 @@
     <slot>
       <n-text depth="2">
         <n-a>{{ $t('global.doc_addr') }}: </n-a>
-        <n-a italic href="http://www.mtruning.club/">
-          http://www.mtruning.club/
+        <n-a italic :href="docPath">
+          {{docPath}}
         </n-a>
       </n-text>
     </slot>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { docPath } from '@/settings/pathConst'
+</script>
 
 <style lang="scss" scoped>
 @include go(footer) {
