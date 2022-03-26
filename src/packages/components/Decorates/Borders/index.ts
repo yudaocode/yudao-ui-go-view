1ae22cafa38a783ad67d46daa99b8736a6b5ffdb
MTrun <1262327911@qq.com>
2022-03-26 23:14:12 +0800

    feat: 新增边框7

--- src/packages/components/Decorates/Borders/index.ts
+++ src/packages/components/Decorates/Borders/index.ts
@@ -4,6 +4,7 @@ import { Border03Config } from './Border03/index'
 import { Border04Config } from './Border04/index'
 import { Border05Config } from './Border05/index'
 import { Border06Config } from './Border06/index'
+import { Border07Config } from './Border07/index'
 
 export default [
   Border01Config,
@@ -11,5 +12,6 @@ export default [
   Border03Config,
   Border04Config,
   Border05Config,
-  Border06Config
+  Border06Config,
+  Border07Config
 ]
