b4aa89e810ecb90d9f45aa37653b92cf3fe9dd3b
奔跑的面条 <1262327911@qq.com>
2022-04-17 15:16:47 +0800

    feat: 新增赞助链接

--- src/settings/pathConst.ts
+++ src/settings/pathConst.ts
@@ -5,3 +5,6 @@ export const docPath = "http://www.mtruning.club:81/"
 
 // 项目源码
 export const giteeSourceCodePath = "https://gitee.com/MTrun/go-view/"
+
+// 赞助
+export const sponsorPath = "http://www.mtruning.club:81/more/sponsor.html"
