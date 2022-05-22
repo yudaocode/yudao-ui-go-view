7f315b95ce17c455c249307f481fbf500a0552a1
奔跑的面条 <1262327911@qq.com>
2022-05-22 14:05:57 +0800

    feat: 新增项目列表接口

--- src/api/path/project.ts
+++ src/api/path/project.ts
@@ -2,6 +2,16 @@ import { http } from '@/api/http'
 import { httpErrorHandle } from '@/utils'
 import { RequestHttpEnum, ModuleTypeEnum } from '@/enums/httpEnum'
 
+// * 项目列表
+export const projectListApi = async (data: object) => {
+  try { 
+    const res = await http(RequestHttpEnum.GET)(`${ModuleTypeEnum.PROJECT}/list`, data);
+    return res;
+  } catch {
+    httpErrorHandle();
+  }
+}
+
 // * 新增项目
 export const createProjectApi = async (data: object) => {
   try { 
