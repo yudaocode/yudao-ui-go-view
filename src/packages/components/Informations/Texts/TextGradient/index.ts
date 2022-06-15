0c5b5f96f1ad068d7bc078c3030a0f6f5e567519
奔跑的面条 <1262327911@qq.com>
2022-06-15 09:46:53 +0800

    chore: 优化渐变文本展示图

--- src/packages/components/Informations/Texts/TextGradient/index.ts
+++ src/packages/components/Informations/Texts/TextGradient/index.ts
@@ -1,4 +1,4 @@
-import image from '@/assets/images/chart/informations/text_static.png'
+import image from '@/assets/images/chart/informations/text_gradient.png'
 import { ConfigType, PackagesCategoryEnum } from '@/packages/index.d'
 import { ChatCategoryEnum,ChatCategoryEnumName } from '../../index.d'
 
