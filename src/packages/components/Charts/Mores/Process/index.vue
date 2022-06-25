012eed6f670ff21f5539e4290b712dd4e392e8c2
奔跑的面条 <1262327911@qq.com>
2022-06-25 15:51:38 +0800

    feat: 新增进度条多个配置项

--- src/packages/components/Charts/Mores/Process/index.vue
+++ src/packages/components/Charts/Mores/Process/index.vue
@@ -1,27 +1,53 @@
 <template>
   <n-progress
     :type="type"
+    :height="h"
+    :processing="processing"
     :percentage="dataset"
     :indicator-placement="indicatorPlacement"
     :color="color"
-  />
+    :rail-color="railColor"
+    :offset-degree="offsetDegree"
+  >
+    <n-text
+      :style="{
+        color: indicatorTextColor,
+        fontSize: `${indicatorTextSize}px`
+      }"
+    >
+      {{dataset}} {{unit}}
+    </n-text>
+  </n-progress>
 </template>
 
 <script setup lang="ts">
 import { PropType, toRefs, watch } from 'vue'
 import { useChartDataFetch } from '@/hooks'
 import { useChartEditStore } from '@/store/modules/chartEditStore/chartEditStore'
-import config  from './config'
+import config from './config'
 
 const props = defineProps({
   chartConfig: {
     type: Object as PropType<config>,
-    required: true,
-  },
+    required: true
+  }
 })
 
 // 取配置数据
-const { type, color, indicatorPlacement, dataset } = toRefs(props.chartConfig.option)
+const { w, h } = toRefs(props.chartConfig.attr)
+const {
+  type,
+  unit,
+  color,
+  fontSize,
+  processing,
+  railColor,
+  indicatorTextColor,
+  indicatorPlacement,
+  indicatorTextSize,
+  offsetDegree,
+  dataset
+} = toRefs(props.chartConfig.option)
 
 useChartDataFetch(props.chartConfig, useChartEditStore)
 </script>
