f47c890d2f942fc73ae78e42bb2566e5760f5590
奔跑的面条 <1262327911@qq.com>
2022-07-21 11:43:42 +0800

    fix: 处理打包 ts 类型错误

--- src/views/chart/ContentConfigurations/components/ChartData/components/ChartDataRequest/components/RequestGlobalHeaderTable/index.vue
+++ src/views/chart/ContentConfigurations/components/ChartData/components/ChartDataRequest/components/RequestGlobalHeaderTable/index.vue
@@ -4,7 +4,13 @@
       <n-tab v-for="item in tabs" :key="item" :name="item" :tab="item"> {{ item }} </n-tab>
     </n-tabs>
     <div class="go-mt-3">
-      <request-header-table :editDisabled="$attrs.editDisabled" :target="requestParams[tabValue]" @update="updateRequestParams"></request-header-table>
+      <!-- 这里的 v-if 是为了处理打包 ts 错类型误 -->
+      <request-header-table
+        v-if="tabValue === RequestParamsTypeEnum.HEADER"
+        :editDisabled="editDisabled"
+        :target="requestParams[tabValue]"
+        @update="updateRequestParams"
+      ></request-header-table>
     </div>
   </div>
 </template>
@@ -14,6 +20,13 @@ import { ref, toRefs } from 'vue'
 import { useTargetData } from '@/views/chart/ContentConfigurations/components/hooks/useTargetData.hook'
 import { RequestHeaderTable } from '../RequestHeaderTable'
 import { RequestParamsTypeEnum, RequestParamsObjType } from '@/enums/httpEnum'
+
+defineProps({
+  editDisabled: {
+    type: Boolean,
+    default: true
+  }
+})
 
 const { chartEditStore } = useTargetData()
 const { requestParams } = toRefs(chartEditStore.getRequestGlobalConfig)
