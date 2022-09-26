ba0cc9cd82cc4ac9ea9b0ec0ee324b113264ac7f
tnt group <dodu@live.cn>
2022-09-26 16:44:02 +0800

    feat: 新增装饰6组件

---
+++ src/packages/components/Decorates/Decorates/Decorates06/index.ts
@@ -0,0 +1,14 @@
+import image from '@/assets/images/chart/decorates/decorates06.png'
+import { ConfigType, PackagesCategoryEnum } from '@/packages/index.d'
+import { ChatCategoryEnum, ChatCategoryEnumName } from '../../index.d'
+
+export const Decorates06Config: ConfigType = {
+  key: 'Decorates06',
+  chartKey: 'VDecorates06',
+  conKey: 'VCDecorates06',
+  title: '装饰-06',
+  category: ChatCategoryEnum.DECORATE,
+  categoryName: ChatCategoryEnumName.DECORATE,
+  package: PackagesCategoryEnum.DECORATES,
+  image
+}
