f22994976cfa21375109ed2ff6670228ddef2251
刘嘉威 <daidaibg@163.com>
2022-10-27 11:01:41 +0800

    feat: 增加胶囊柱图

---
+++ src/packages/components/Charts/Mores/CapsuleChart/config.ts
@@ -0,0 +1,23 @@
+import {  PublicConfigClass } from '@/packages/public'
+import { CapsuleChartConfig } from './index'
+import { CreateComponentType } from '@/packages/index.d'
+import { chartInitConfig } from '@/settings/designSetting'
+
+import cloneDeep from 'lodash/cloneDeep'
+import dataJson from './data.json'
+
+
+export const option = {
+  dataSet:dataJson,
+  colors: ['#e062ae', '#fb7293', '#e690d1', '#32c5e9', '#96bfff'],
+  unit: '',
+  itemHeight:10,
+  showValue: true
+}
+
+export default class Config extends PublicConfigClass implements CreateComponentType {
+  public key: string = CapsuleChartConfig.key
+  public attr = { ...chartInitConfig,w: 300, h: 200 ,zIndex: -1}
+  public chartConfig = cloneDeep(CapsuleChartConfig)
+  public option = cloneDeep(option)
+}
